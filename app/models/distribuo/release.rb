3dda6449168c22ac78bc60ccd98577daf9a80b17
Syed Waqas Ali Shah <s.waqas@rapidlogistics.nl>
2023-07-18 15:44:47 +0400

    revert fk mobilis_release_id to distribuo_release_id

--- app/models/distribuo/release.rb
+++ app/models/distribuo/release.rb
@@ -3,7 +3,7 @@ module Distribuo
     has_one_attached :build
 
     belongs_to :app
-    has_many :downloads, class_name: 'Download', foreign_key: 'mobilis_release_id', dependent: :destroy
+    has_many :downloads, class_name: 'Download', foreign_key: 'distribuo_release_id', dependent: :destroy
 
     scope :administrable, -> { all }
 
@@ -15,11 +15,11 @@ module Distribuo
     def download_url(controller, from_manifest: false)
       if app.operating_system == 'ios' && from_manifest == false
         url = controller.distribuo.app_release_manifest_url(app_id: app.id, release_id: id, host: controller.request.host,
-                                                          protocol: controller.request.protocol, format: :plist)
+                                                            protocol: controller.request.protocol, format: :plist)
         "itms-services://?action=download-manifest&url=#{url}"
       else
         controller.distribuo.app_release_download_url(app, self, host: controller.request.host,
-                                                               protocol: controller.request.protocol)
+                                                                 protocol: controller.request.protocol)
       end
     end
 
